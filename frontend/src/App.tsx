277938b400c5d71fa631ff80645ecb796c40d8a5
Ben Rolf <benrolf70@gmail.com>
2021-11-27 22:34:26 -0500

    started bottom bar code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BottomBar } from '../src/components/bottomBar/bottombar'
 
 function App() {
+
+  const homeOnClick = () => {
+    console.log('on home click');
+  }
+  const historyOnClick = () => {
+    console.log('on history click');
+  }
+  const settingsOnClick = () => {
+    console.log('on settings click');
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <BottomBar
+      homeOnClick={homeOnClick}
+      historyOnClick={historyOnClick}
+      settingsOnClick={settingsOnClick}></BottomBar>
       </header>
     </div>
   );
