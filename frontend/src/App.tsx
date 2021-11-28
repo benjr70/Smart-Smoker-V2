0d9aa40df5622f294b2b9b4b85e91b48d453867b
Ben Rolf <benrolf70@gmail.com>
2021-11-27 23:26:21 -0500

    can now use buttons on bottom to change home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,58 @@
 import React from 'react';
 import './App.css';
 import { BottomBar } from '../src/components/bottomBar/bottombar'
+import { Screens } from './components/common/interfaces/enums';
+import { history } from './components/history/history';
+import { home } from './components/home/home';
+import { settings } from './components/settings/settings';
 
-function App() {
-
-  const homeOnClick = () => {
-    console.log('on home click');
-  }
-  const historyOnClick = () => {
-    console.log('on history click');
-  }
-  const settingsOnClick = () => {
-    console.log('on settings click');
+class App extends React.Component<{},{currentScreen: Screens}>{
+  constructor(props: any) {
+    super(props);
+    this.homeOnClick = this.homeOnClick.bind(this);
+    this.historyOnClick = this.historyOnClick.bind(this);
+    this.settingsOnClick = this.settingsOnClick.bind(this);
+    this.state = {currentScreen: Screens.HOME};
   }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-      <BottomBar
-      homeOnClick={homeOnClick}
-      historyOnClick={historyOnClick}
-      settingsOnClick={settingsOnClick}></BottomBar>
-      </header>
-    </div>
-  );
+   homeOnClick() {
+    this.setState({currentScreen: Screens.HOME})
+  }
+   historyOnClick() {
+    this.setState({currentScreen: Screens.HISTORY})
+  }
+   settingsOnClick() {
+    this.setState({currentScreen: Screens.SETTINGS})
+  }
+    
+  render(){
+
+    let screen;
+    switch(this.state.currentScreen){
+      case Screens.HOME:
+        screen = home();
+        break;
+      case Screens.HISTORY:
+        screen = history();
+        break;
+      case Screens.SETTINGS:
+        screen = settings();
+        break;
+    }
+  
+      return (
+      <div className="App">
+        <header className="App-header">
+        {screen}
+        <BottomBar
+        homeOnClick={this.homeOnClick}
+        historyOnClick={this.historyOnClick}
+        settingsOnClick={this.settingsOnClick}></BottomBar>
+        </header>
+      </div>
+    );
+  }
+
 }
 
 export default App;
