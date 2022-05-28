7f342b834678d6833c6ce93c6e6d73cc866dd5f0
Ben Rolf <benrolf70@gmail.com>
2022-05-28 13:31:51 -0400

    serial read attempt

--- smoker/src/components/home/home.tsx
+++ smoker/src/components/home/home.tsx
@@ -1,21 +1,49 @@
 import React from 'react';
 import './home.style.css'
 import Grid from '@mui/material/Grid';
+import { readTemp } from '../../services/readSerial';
 
-export class Home extends React.Component<{}> {
+interface State {
+    meatTemp: number;
+    chamberTemp: number;
+}
+export class Home extends React.Component<{}, {tempState: State}> {
+
+    constructor(props: any) {
+        super(props);
+        this.state = { tempState: {
+            meatTemp: 0,
+            chamberTemp: 0
+            }
+        };
+    }
 
 
-
+    componentDidMount(){
+        readTemp().on('data', (data) => {
+            console.log(data);
+        })
+    }
 
     render(): React.ReactNode { 
         return (
         <Grid container className='background'>
-            <Grid container direction="column" spacing={2}>
-                <Grid item className='text' >
-                   Meat Temp
+            <Grid container direction="column">
+                <Grid container direction="row"  spacing={2}>
+                    <Grid item xs={3} className='text' >
+                        Meat Temp
+                    </Grid>
+                    <Grid item className='text' >
+                        {this.state.tempState.meatTemp}
+                    </Grid>
                 </Grid>
-                <Grid item className='text'>
-                    Chamber Temp
+                <Grid container direction="row" spacing={2}>
+                    <Grid item xs={3} className='text' >
+                        Chamber Temp
+                    </Grid>
+                    <Grid item className='text' >
+                        {this.state.tempState.chamberTemp}
+                    </Grid>
                 </Grid>
             </Grid>
         </Grid>)
--- smoker/src/services/readSerial.ts
+++ smoker/src/services/readSerial.ts
@@ -1,11 +1,11 @@
 import { SerialPort } from 'serialport';
 
-export const readTemp = () => {
+export const readTemp = (): SerialPort<any> => {
 
     const port = new SerialPort({
         path: '/dev/ttyUSB0',
         baudRate: 9600,
     });
 
-    
+    return port;
 }
