f99d5c5d35950a7521c88c8194d7edc9c98182be
Ben Rolf <benrolf70@gmail.com>
2022-05-28 14:13:52 -0400

    added parser

--- smoker/src/services/readSerial.ts
+++ smoker/src/services/readSerial.ts
@@ -1,11 +1,13 @@
-import { SerialPort } from 'serialport';
+import {  SerialPort } from 'serialport'
 
 export const readTemp = (): SerialPort<any> => {
 
+    const ReadLine = require('@serialport/parser-readline');
     const port = new SerialPort({
         path: '/dev/ttyUSB0',
         baudRate: 9600,
     });
 
-    return port;
+    const parser = new ReadLine();
+    return port.pipe(parser);
 }
