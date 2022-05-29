060baae2dedc2e59bfdcaed87cad8437c7e84005
Ben Rolf <benrolf70@gmail.com>
2022-05-29 14:17:14 -0400

    added websocket 5

--- smoker/serialTowebsocket.py
+++ smoker/serialTowebsocket.py
@@ -1,35 +1,37 @@
-#!/usr/bin/python3.9
-import time
+#!/usr/bin/python
+ 
 import serial
-import re
 import asyncio
+import datetime
+import random
 import websockets
-
-ser =  serial.Serial( port = '/dev/ttyUSB0',
-                      baudrate = 9600,
-                      parity = serial.PARITY_NONE,
-                      stopbits = serial.STOPBITS_ONE,
-                      bytesize = serial.EIGHTBITS,
-                      timeout = 1
-                      )
-
-
-async def handler(websocket, path):
  
-    # data = await websocket.recv()
+ser = serial.Serial(
+port='/dev/ttyUSB0',\
+baudrate=9600,\
+parity=serial.PARITY_NONE,\
+stopbits=serial.STOPBITS_ONE,\
+bytesize=serial.EIGHTBITS,\
+timeout=0)
  
-    # reply = f"Data recieved as:  {data}!"
-	
-	await websocket.send(ser.readline().decode('utf-8'))
-	line = ser.readline().decode('utf-8')
-	print(line)
+print("connected to: " + ser.portstr)
  
  
+async def tx(websocket, path):
+    line = []
+    while True:
+        for i in ser.read():
+            c = chr(i)
+            line.append(c)
+            if c == '\n':
+                print(''.join(line))
+                await websocket.send(''.join(line))
+                line = []
+                break
  
-start_server = websockets.serve(handler, "localhost", 8000)
- 
- 
+start_server = websockets.serve(tx, "127.0.0.1", 5678)
  
 asyncio.get_event_loop().run_until_complete(start_server)
- 
 asyncio.get_event_loop().run_forever()
+ 
+ser.close()
