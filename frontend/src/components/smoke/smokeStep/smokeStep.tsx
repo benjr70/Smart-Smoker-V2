6d93bcf1ba40ffec5879b1f9c80633968b58e0ae
Ben Rolf <benrolf70@gmail.com>
2022-09-01 22:28:40 -0400

    more url change

--- frontend/src/components/smoke/smokeStep/smokeStep.tsx
+++ frontend/src/components/smoke/smokeStep/smokeStep.tsx
@@ -45,8 +45,8 @@ export class SmokeStep extends React.Component<{}, {tempState: State}> {
     }
 
     componentDidMount(): void {
-        // const socket = io('http://136.60.164.223:3001');
-        const socket = io('http://localhost:3001');
+        const socket = io('http://136.60.164.223:3001');
+        //const socket = io('http://localhost:3001');
         console.log(socket);
         socket.on('events', ((message) => {
             let tempObj = JSON.parse(message);
