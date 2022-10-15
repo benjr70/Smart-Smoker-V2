c2ae3dd55a09f43ba2efcd4e0ecf1983ba26f881
Ben Rolf <benrolf70@gmail.com>
2022-10-15 13:50:01 -0400

    oops

--- smoker/src/components/home/home.tsx
+++ smoker/src/components/home/home.tsx
@@ -18,7 +18,7 @@ interface State {
 
 let initTemps: TempData[] = [];
 let socket: any;
-let buffer: State[];
+let buffer: State[] = [];
 export class Home extends React.Component<{}, {tempState: State}> {
 
     constructor(props: any) {
@@ -63,12 +63,13 @@ export class Home extends React.Component<{}, {tempState: State}> {
                 //push to Q
                 buffer.unshift(temp);
                 // Q lenght is > 1 loop
+                console.log('try',buffer.length ,buffer);
                 while(buffer.length >= 1){
                     socket.emit('events', JSON.stringify(buffer[buffer.length - 1]));
                     buffer.pop();
                 }
             } catch(e) {
-                console.log(e);
+                console.log('catch',buffer.length ,buffer);
             }
         }
         
