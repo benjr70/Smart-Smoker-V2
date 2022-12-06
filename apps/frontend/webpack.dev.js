fee87c4720bd0676085182b67b90194ac526b369
Ben Rolf <benrolf70@gmail.com>
2022-12-05 19:26:40 -0500

    fix env file and prod build for cloud frontend

---
+++ apps/frontend/webpack.dev.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = (env) = {
+  mode: 'development',
+  entry: './src/index.tsx',
+  devtool: 'inline-source-map',
+  target: 'web',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js'
+  },
+    devtool: 'inline-source-map',
+    devServer: {
+    static: './dist',
+  },
+  module: {
+  rules: [
+    {
+      test: /\.tsx?$/,
+      use: 'ts-loader',
+      exclude: /node_modules/,
+    },
+    {
+      test: /\.css$/,
+      use: ["style-loader", "css-loader"],
+    }
+  ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins:[
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    }),
+    new Dotenv({
+      path: '.env.local'
+    }),
+  ]
+}
