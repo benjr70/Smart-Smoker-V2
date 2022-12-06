bcc8c19254533c5969cddc6070fcfe558e418c05
Ben Rolf <benrolf70@gmail.com>
2022-12-05 19:34:38 -0500

    added env files and prod build for smoker

--- apps/smoker/webpack.prod.js
+++ apps/smoker/webpack.prod.js
@@ -3,7 +3,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
+  devtool: 'source-map',
   entry: './src/index.tsx',
   devtool: 'inline-source-map',
   target: 'web',
@@ -35,6 +36,8 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './public/index.html'
     }),
-    new Dotenv(),
+    new Dotenv({
+      path: '.env.prod'
+    }),
   ]
 }
