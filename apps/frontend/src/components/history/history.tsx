6ee97858f91d98371f4fe6dc96fe2d8cdf152f29
Ben Rolf <benrolf70@gmail.com>
2023-03-20 21:19:16 -0400

    added smoke card component

--- apps/frontend/src/components/history/history.tsx
+++ apps/frontend/src/components/history/history.tsx
@@ -1,5 +1,37 @@
 import React from 'react'
+import { SmokeCard } from './smokeCard/smokeCard';
+import { Grid, TextField } from '@mui/material';
+import './history.style.css';
 
 export const history = () => {
-    return <>history</>;
+    return (
+    <Grid>
+        {/* <Grid container spacing={2} sx={{display: 'flex', justifyContent: 'center'}}>
+            <Grid item xs={11}>
+                <TextField
+                    label='Search'
+                /> 
+            </Grid>
+        </Grid> */}
+        <Grid container spacing={2} sx={{display: 'flex', justifyContent: 'center'}}>
+            <Grid item xs={11}>
+                <SmokeCard
+                    name='Smoke Name'
+                    meatType='Brisket'
+                    date='03/20/2023'
+                    weight='4'
+                    weightUnit='LB'
+                />
+            </Grid>
+            <Grid item xs={11}>
+                <SmokeCard
+                    name='Smoke Name'
+                    meatType='Brisket'
+                    date='03/20/2023'
+                    weight='4'
+                    weightUnit='LB'
+                />
+            </Grid>
+        </Grid>
+    </Grid>);
 }
---
+++ apps/frontend/src/components/history/smokeCard/smokeCard.tsx
@@ -0,0 +1,50 @@
+import { Button, Card, CardActions, CardContent, Grid, ThemeProvider, Typography, createTheme } from "@mui/material";
+import React from "react";
+
+
+interface SmokeCardProps {
+    name: string
+    meatType: string
+    date: string
+    weight: string
+    weightUnit: string
+}
+
+const theme = createTheme({
+    components: {
+        MuiCard: {
+            styleOverrides: {
+                root: {
+                    backgroundColor: 'white',
+                    borderRadius: '15px'
+                }
+            }
+        }
+    }
+})
+
+
+
+export function SmokeCard(props: SmokeCardProps): JSX.Element {
+    return (
+    <Grid >
+        <ThemeProvider theme={theme}>
+        <Card className="card">
+            <CardContent >
+                <Typography variant="h5" component="div">
+                    {props.name}
+                </Typography>
+                <Typography sx={{ fontSize: 14 }} color="text.secondary">
+                    {props.weight}{props.weightUnit} {props.meatType}
+                </Typography>
+                <Typography sx={{ fontSize: 14 }} color="text.secondary">
+                    {props.date}
+                </Typography>
+                <CardActions>
+                    <Button size="small">View</Button>
+                </CardActions>
+            </CardContent>
+        </Card>
+        </ThemeProvider>
+    </Grid>);
+}
