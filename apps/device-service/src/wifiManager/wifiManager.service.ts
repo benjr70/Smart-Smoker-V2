9a41a0a069fcd750d3575bd3934f4dfa0ba5f6d1
Ben Rolf <benrolf70@gmail.com>
2023-06-28 06:57:55 -0400

    pares error test again

--- apps/device-service/src/wifiManager/wifiManager.service.ts
+++ apps/device-service/src/wifiManager/wifiManager.service.ts
@@ -21,11 +21,10 @@ export class WifiManagerService {
     async connectToWiFi(dto: wifiDto) {
         await wifi.scan();
          return await wifi.connect({ssid: dto.ssid, password: dto.password}).catch(err => {
-            const newString = err.message.split('\n');
-            console.log('here', newString);
+            const parsedError = err.message.split('\n');
             throw new HttpException({
                 status: HttpStatus.BAD_REQUEST,
-                error: err.message,
+                error: parsedError[1],
             }, HttpStatus.BAD_REQUEST);
          });
     }
