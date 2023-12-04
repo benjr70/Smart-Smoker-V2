1ba196d09d56bb72160d165d02b77511c7cd5925
Ben Rolf <benrolf70@gmail.com>
2023-12-04 07:34:28 -0500

    microcontroller code for four temp probes

--- MicroController/MicroController.ino
+++ MicroController/MicroController.ino
@@ -1,10 +1,11 @@
-
-#define     THERMISTOR_PIN0     0      // Pin between the thermistor and 
-                                      // series resistor.
-
 #define     THERMISTOR_PIN1     1      // Pin between the thermistor and 
                                       // series resistor.
-
+#define     THERMISTOR_PIN2     2      // Pin between the thermistor and 
+                                      // series resistor.
+#define     THERMISTOR_PIN3     3      // Pin between the thermistor and 
+                                      // series resistor.
+#define     THERMISTOR_PIN4     4      // Pin between the thermistor and 
+                                      // series resistor.                                                                      
 #define     SERIES_RESISTOR    10000  // Series resistor value in ohms.
 
 #define     USE_FAHRENHEIT     true   // True to use Fahrenheit, false to
@@ -25,10 +26,14 @@ float B = 0.000262156391;
 float C = -0.000000024874;
 
 
-float meatAvg [5];
 float chamberAvg [5];
-float newMeatTemp;
+float meatAvg [5];
+float meatAvg2 [5];
+float meatAvg3 [5];
 float newChamberTemp;
+float newMeatTemp;
+float newMeatTemp2;
+float newMeatTemp3;
 int startCount = 0;
   
 void setup() {
@@ -38,16 +43,11 @@ void setup() {
 
 void loop() {
 
-  newMeatTemp = kelvinToFahrenheit(readTemp(THERMISTOR_PIN0));
-  newChamberTemp = kelvinToFahrenheit(readTemp(THERMISTOR_PIN1));
 
-  if(!(newMeatTemp > meatAvg[0] + 50) && !(newMeatTemp < meatAvg[0] - 50) || startCount < 5){
-    meatAvg[4] = meatAvg[3];
-    meatAvg[3] = meatAvg[2];
-    meatAvg[2] = meatAvg[1];
-    meatAvg[1] = meatAvg[0];
-    meatAvg[0] = newMeatTemp;
-  }
+  newChamberTemp = kelvinToFahrenheit(readTemp(THERMISTOR_PIN1));
+  newMeatTemp = kelvinToFahrenheit(readTemp(THERMISTOR_PIN2));
+  newMeatTemp2 = kelvinToFahrenheit(readTemp(THERMISTOR_PIN3));
+  newMeatTemp3 = kelvinToFahrenheit(readTemp(THERMISTOR_PIN4));
    
   if(!(newChamberTemp > chamberAvg[0] + 50) && !(newChamberTemp < chamberAvg[0] - 50)  || startCount < 5){
     chamberAvg[4] = chamberAvg[3];
@@ -57,9 +57,40 @@ void loop() {
     chamberAvg[0] = newChamberTemp;
   }
 
+
+  if(!(newMeatTemp > meatAvg[0] + 50) && !(newMeatTemp < meatAvg[0] - 50) || startCount < 5){
+    meatAvg[4] = meatAvg[3];
+    meatAvg[3] = meatAvg[2];
+    meatAvg[2] = meatAvg[1];
+    meatAvg[1] = meatAvg[0];
+    meatAvg[0] = newMeatTemp;
+  }
+
+  if(!(newMeatTemp2 > meatAvg2[0] + 50) && !(newMeatTemp2 < meatAvg2[0] - 50) || startCount < 5){
+    meatAvg2[4] = meatAvg2[3];
+    meatAvg2[3] = meatAvg2[2];
+    meatAvg2[2] = meatAvg2[1];
+    meatAvg2[1] = meatAvg2[0];
+    meatAvg2[0] = newMeatTemp2;
+  }
+
+  if(!(newMeatTemp3 > meatAvg3[0] + 50) && !(newMeatTemp3 < meatAvg3[0] - 50) || startCount < 5){
+    meatAvg3[4] = meatAvg3[3];
+    meatAvg3[3] = meatAvg3[2];
+    meatAvg3[2] = meatAvg3[1];
+    meatAvg3[1] = meatAvg3[0];
+    meatAvg3[0] = newMeatTemp3;
+  }
+
   if(startCount > 5){
     Serial.print("{\"Meat\": \"");
     Serial.print(getAvg(meatAvg));
+    Serial.print("\",");
+    Serial.print("\"Meat2\": \"");
+    Serial.print(getAvg(meatAvg2));
+    Serial.print("\",");
+    Serial.print("\"Meat3\": \"");
+    Serial.print(getAvg(meatAvg3));
     Serial.print("\",");
     Serial.print("\"Chamber\": \"");
     Serial.print(getAvg(chamberAvg));
