5b9edc6685cafa9adf9d75252ca55b01c4abc454
Ben Rolf <benrolf70@gmail.com>
2023-12-15 18:56:27 -0500

    added e2e dev env for device service

--- apps/device-service/src/serial/serial.serivce.ts
+++ apps/device-service/src/serial/serial.serivce.ts
@@ -3,6 +3,16 @@ import { Subject } from "rxjs";
 import { ReadlineParser, SerialPort } from "serialport";
 
 
+let NODE_ENV
+
+// interface for temp data
+interface TempData {
+  Meat: number;
+  Meat2: number;
+  Meat3: number;
+  Chamber: number;
+}
+
 @Injectable()
 export class SerialService {
     
@@ -10,18 +20,36 @@ export class SerialService {
     private dataSubject: Subject<string> = new Subject<string>();
 
     constructor(){
-         this.port = new SerialPort({
-            path: '/dev/ttyS0',
-            baudRate: 9600,
-        });
+        NODE_ENV = process.env.NODE_ENV ? process.env.NODE_ENV.trim() : '';
+
 
         const parser = new ReadlineParser();
-        this.port.pipe(parser);
-        parser.on('data', (data) => {
-            const stringData = data.toString('utf-8');
-            this.handleTempLogging(stringData);
-            this.dataSubject.next(stringData);
-        });
+        if(NODE_ENV === 'local'){
+          Logger.log('Running in emulator mode', 'SerialService');
+          let stringData: TempData = {
+            Meat: 0,
+            Meat2: 0,
+            Meat3: 0,
+            Chamber: 0,
+          };
+          setInterval(() => {
+            stringData = this.generateTempString(stringData);
+            this.handleTempLogging(JSON.stringify(stringData));
+            this.dataSubject.next(JSON.stringify(stringData));
+          }, 500);
+        } else {
+          Logger.log('Running in production mode', 'SerialService');
+          this.port = new SerialPort({
+            path: '/dev/ttyS0',
+            baudRate: 9600,
+          });
+          this.port.pipe(parser);
+          parser.on('data', (data) => {
+              const stringData = data.toString('utf-8');
+              this.handleTempLogging(stringData);
+              this.dataSubject.next(stringData);
+          });
+      }
     }
     
 
@@ -42,4 +70,28 @@ export class SerialService {
         Logger.warn(`temps too hot: ${JSON.stringify(tempObj)}`, 'SerialService');
       }
     }
+
+
+    // function that increase temps based off of input temps 
+    generateTempString(tempObj: TempData): TempData {
+      // if any temp gets above 500, reset to 0
+      let newTempObj: TempData;
+      if(tempObj.Meat > 500 || tempObj.Meat2 > 500 || tempObj.Meat3 > 500 || tempObj.Chamber > 500){
+        newTempObj = {
+          Meat: 0,
+          Meat2: 0,
+          Meat3: 0,
+          Chamber: 0,
+        }
+      } else {
+         newTempObj = {
+            Meat: tempObj.Meat + 1,
+            Meat2: tempObj.Meat2 + 2,
+            Meat3: tempObj.Meat3 + 3,
+            Chamber: tempObj.Chamber + 4,
+          }
+        }
+      return (newTempObj);
+    }
+
 }
