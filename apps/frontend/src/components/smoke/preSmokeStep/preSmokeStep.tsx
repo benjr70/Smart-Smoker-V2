da87078c3f1c9e89fe188b75e0386afb4f63ae72
Ben Rolf <benrolf70@gmail.com>
2024-11-24 10:13:37 -0500

    refactor pre-smoke step

--- apps/frontend/src/components/smoke/preSmokeStep/preSmokeStep.tsx
+++ apps/frontend/src/components/smoke/preSmokeStep/preSmokeStep.tsx
@@ -1,5 +1,5 @@
 import { Autocomplete, Grid, MenuItem, Select, TextField } from "@mui/material";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { getCurrentPreSmoke, setCurrentPreSmoke } from "../../../Services/preSmokeService";
 import { DynamicList } from "../../common/components/DynamicList";
 import { WeightUnits } from "../../common/interfaces/enums";
@@ -7,9 +7,9 @@ import { preSmoke } from "../../common/interfaces/preSmoke";
 import './preSmokeStep.style.css'
 
 const meats = [
-    'ribs',
-    'brisket',
-    'turkey',
+    'Ribs',
+    'Brisket',
+    'Turkey',
 ];
 
 type PreSmokeStepProps = {
@@ -17,166 +17,98 @@ type PreSmokeStepProps = {
   };
   
 
-export class PreSmokeStep extends React.Component<PreSmokeStepProps,{preSmokeState: preSmoke}> {
-    constructor(props: any){
-        super(props);
-        this.state = { preSmokeState: {
-            name: '',
-            meatType: '',
-            weight: {
-                unit: WeightUnits.LB
-            },
-            steps: [''],
-            notes: ''
-        }};
-       this.handleUnitChange = this.handleUnitChange.bind(this);
-       this.updateName = this.updateName.bind(this);
-       this.updateMeatType = this.updateMeatType.bind(this);
-       this.onNextClick = this.onNextClick.bind(this);
-       this.updateWeight = this.updateWeight.bind(this);
-       this.updateNotes = this.updateNotes.bind(this);
-       this.newLine = this.newLine.bind(this);
-       this.removeLine = this.removeLine.bind(this);
-       this.updateSteps = this.updateSteps.bind(this);
-    }
+export function PreSmokeStep (props: PreSmokeStepProps) {
 
-    componentDidMount(): void {
+    const [preSmokeState, setPreSmokeState] = React.useState<preSmoke>({
+        name: '',
+        meatType: '',
+        weight: {
+            unit: WeightUnits.LB
+        },
+        steps: [''],
+        notes: ''
+    })
+    const latestState = useRef(preSmokeState);
+
+    useEffect(() => {
+        latestState.current = preSmokeState;
+      }, [preSmokeState]);
+    
+    useEffect(() => {
         getCurrentPreSmoke().then(result => {
             if(result){
-                this.setState({preSmokeState: result});
+                setPreSmokeState(result);
             }
          })
-    }
+         return () => {
+             setCurrentPreSmoke(latestState.current);
+         }
+    }, [])
 
-    handleUnitChange( event: any) {
-        let temp = this.state.preSmokeState;
-        switch(event.target.value){
-            case 'LB':
-                temp.weight.unit = WeightUnits.LB
-                this.setState({preSmokeState: temp})
-                break;
-            case 'OZ':
-                temp.weight.unit = WeightUnits.OZ
-                this.setState({preSmokeState: temp})
-                break;
-        }
-    };
-
-    updateName(event: any){
-        let temp = this.state.preSmokeState;
-        temp.name = event.target.value;
-        this.setState({preSmokeState: temp});
-    }
-
-    updateMeatType(newInputValue: string){
-        let temp = this.state.preSmokeState;
-        temp.meatType = newInputValue;
-        this.setState({preSmokeState: temp});
-    }
-
-    onNextClick() {
-        setCurrentPreSmoke(this.state.preSmokeState);
-    }
-
-    componentWillUnmount() {
-        setCurrentPreSmoke(this.state.preSmokeState);
-    }
-
-    updateWeight(event: any){
-        let temp = this.state.preSmokeState;
-        temp.weight.weight = event.target.value;
-        this.setState({preSmokeState: temp});
-    }
-
-    updateNotes(event: any){
-        let temp = this.state.preSmokeState;
-        temp.notes = event.target.value;
-        this.setState({preSmokeState: temp});
-    }
-    
-     newLine() {
-        let temp = this.state.preSmokeState;
-        temp.steps = [...temp.steps, ''];
-        this.setState({preSmokeState: temp});
-    }
-
-     removeLine(index: number){
-        let temp = this.state.preSmokeState;
-        temp.steps.splice(index, 1);
-        this.setState({preSmokeState: temp});
-    }
-    
-     updateSteps(value: string, index: number){
-        let temp = this.state.preSmokeState;
-        temp.steps[index] = value;
-        this.setState({preSmokeState: temp});
-    }
-
-    render(): React.ReactNode {
-        return (
-        <Grid item xs={11} flexDirection='column'>
+    return (
+    <Grid item xs={11} flexDirection='column'>
+        <TextField
+            sx={{marginBottom: '10px'}}
+            id="standard-basic" 
+            label="Name" 
+            variant="standard" 
+            value={preSmokeState.name}
+            onChange={(event: any) => setPreSmokeState({...preSmokeState, name: event.target.value})}
+        />
+        <Autocomplete
+            sx={{marginBottom: '10px'}}
+            freeSolo
+            options={meats.map((option) => option)}
+            inputValue={preSmokeState.meatType}
+            onInputChange={(event, newInputValue) => {setPreSmokeState({...preSmokeState, meatType: newInputValue})}}
+            renderInput={(params) => <TextField  {...params}label="Meat Type"  />}
+        />
+        <Grid className="weight">
             <TextField
-                sx={{marginBottom: '10px'}}
+                sx={{marginBottom: '10px', marginRight: '10px'}} 
+                type='number' 
                 id="standard-basic" 
-                label="Name" 
-                variant="standard" 
-                value={this.state.preSmokeState.name}
-                onChange={this.updateName}
+                label="Weight"
+                variant="standard"
+                value={preSmokeState.weight.weight ? preSmokeState.weight.weight: ''}
+                onChange={(event: any) => setPreSmokeState({...preSmokeState, weight: {...preSmokeState.weight, weight: event.target.value}})}
             />
-            <Autocomplete
+            <Select
                 sx={{marginBottom: '10px'}}
-                freeSolo
-                options={meats.map((option) => option)}
-                inputValue={this.state.preSmokeState.meatType}
-                onInputChange={(event, newInputValue) => {this.updateMeatType(newInputValue)}}
-                renderInput={(params) => <TextField  {...params}label="Meat Type"  />}
-            />
-            <Grid className="weight">
-                <TextField
-                    sx={{marginBottom: '10px', marginRight: '10px'}} 
-                    type='number' 
-                    id="standard-basic" 
-                    label="Weight"
-                    variant="standard"
-                    value={this.state.preSmokeState.weight.weight ? this.state.preSmokeState.weight.weight: ''}
-                    onChange={this.updateWeight}
-                />
-                <Select
-                    sx={{marginBottom: '10px'}}
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    value={this.state.preSmokeState.weight.unit}
-                    label="Age"
-                    onChange={this.handleUnitChange}
-                >
-                    <MenuItem value={WeightUnits.LB}>LB</MenuItem>
-                    <MenuItem value={WeightUnits.OZ}>OZ</MenuItem>
-                </Select>
-            </Grid>
-            <Grid flexDirection='column'>
-                <DynamicList
-                    newline ={() => {this.newLine()}}
-                    removeLine={(index) => {this.removeLine(index)}}
-                    steps={this.state.preSmokeState.steps}
-                    onListChange={(step, index) => this.updateSteps(step, index)} />
-            </Grid>
-            <TextField
-               sx={{
-                    marginTop: '10px',
-                    marginBottom: '10px',
-                    width: '100%'
-                }}
-                id="outlined-multiline-static"
-                label="Notes"
-                multiline
-                value={this.state.preSmokeState.notes}
-                onChange={this.updateNotes}
-                rows={4}
-            />
-            <Grid container flexDirection='row-reverse'>
-                {this.props.nextButton}
-            </Grid>
-        </Grid>)
-    }
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={preSmokeState.weight.unit}
+                label="Age"
+                onChange={(event: any) => setPreSmokeState({...preSmokeState, weight: {...preSmokeState.weight, unit: event.target.value}})}
+            >
+                <MenuItem value={WeightUnits.LB}>LB</MenuItem>
+                <MenuItem value={WeightUnits.OZ}>OZ</MenuItem>
+            </Select>
+        </Grid>
+        <Grid flexDirection='column'>
+            <DynamicList
+                newline = {() => setPreSmokeState({...preSmokeState, steps: [...preSmokeState.steps, '']})}
+                removeLine = {(index: number) => setPreSmokeState({...preSmokeState, steps: preSmokeState.steps.filter((_, i) => i !== index)})}
+                steps={preSmokeState.steps}
+                onListChange={(step, index) => setPreSmokeState({...preSmokeState, steps: preSmokeState.steps.map((s, i) => i === index ? step : s)})} />
+        </Grid>
+        <TextField
+            sx={{
+                marginTop: '10px',
+                marginBottom: '10px',
+                width: '100%'
+            }}
+            id="outlined-multiline-static"
+            label="Notes"
+            multiline
+            value={preSmokeState.notes}
+            onChange={(event: any) => setPreSmokeState({...preSmokeState, notes: event.target.value})}
+            rows={4}
+        />
+        <Grid container flexDirection='row-reverse'>
+            {props.nextButton}
+        </Grid>
+    </Grid>)
+    
 }
 
